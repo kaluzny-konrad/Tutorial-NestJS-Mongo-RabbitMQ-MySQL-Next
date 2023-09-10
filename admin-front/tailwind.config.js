e8d3a3ee5df5e463ae65b44672cb85e9d3e46aff
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2023-09-10 17:20:23 +0200

    failed dockerize admin-front

--- admin-front/tailwind.config.js
+++ admin-front/tailwind.config.js
@@ -3,6 +3,7 @@ const {nextui} = require("@nextui-org/react");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
